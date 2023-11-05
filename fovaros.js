33969fd8d8e3bcd75f7b131eb2ab0d6bb46de260
Marianna39 <buraimarianna@gmail.com>
2023-11-05 17:14:25 +0100

    js félig kész, eddig az önálló

--- fovaros.js
+++ fovaros.js
@@ -1,20 +1,77 @@
-let orszagnev;
-let varosnev;
+let jo = 0;
+let osszes = 0;
+let valasz = document.querySelector("#valasz");
 
-let orszag = ["Magyaroszág", "Németország", "Ausztria", "Csehország", 
-            "Spanyolország", "Olaszország", "Svédország", "Finnország", "Franciaország", "Egyesült Királyság"];
+const orszag = ["Magyaroszág", "Németország", "Ausztria", "Csehország", 
+            "Spanyolország", "Olaszország", "Svédország", "Finnország", 
+            "Franciaország", "Egyesült Királyság"];
 
-let varos = ["Budapest", "Berlin", "Bécs", "Prága", "Madrid", 
+const varos = ["Budapest", "Berlin", "Bécs", "Prága", "Madrid", 
             "Róma", "Stockholm", "Helsinki", "Párizs", "London"];
 
 function kerdez() {
-    let mit = orszag[Math.floor(Math.random()*10)];
-    document.querySelector(".orszag").innerHTML = mit;
+       let kerdes = orszag[Math.floor(Math.random()*10)];
+        document.querySelector(".orszag").innerHTML = kerdes;
+            };
+            
+kerdez();
+
+function ellenorzes() {
+    valasz = "";
+    if (valasz.value == "Budapest") { 
+        document.querySelector("#uzenet").innerHTML = "Helyes!";
+        jo += 1;
+    } else {
+        document.querySelector("#uzenet").innerHTML = "Helytelen!";
+        kiir();
+    };
+
+
+
+    osszes += 1;
+    document.querySelector("#talalat").innerHTML = osszes + " / " + jo;
+   
+    setTimeout(torles, 1000);
+  
+};
+
+function torles() {
+    kerdez();
+    valasz.value = "";
 }
 
-/*function ellenoriz() {
-    if (i= orszag.length && i= víros.length) {
-        document.querySelector(".orszag").innerHTML = mit
-    } 
-}*/
-kerdez();
+function kiir() {
+    let orszag = "Magyarország"; //ide kell meghívni a random országot
+    let fovaros = "Budapest"; //ide kell meghívni a hozzá tartotzó várost
+    uzenet.innerHTML += " " + orszag + "<p> fővárosa </p>" + fovaros + ".";
+};
+
+document.querySelector("#ellenoriz").onclick = ellenorzes;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
